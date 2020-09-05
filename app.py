f8b6f3130a54f607fa59e9b6df53fc98750ff804
Michael Zhao <michaelzhao314@gmail.com>
2020-09-05 17:17:04 -0500

    better parsing and comments

--- app.py
+++ app.py
@@ -13,22 +13,22 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 
 
 def main():
-    print("Authenticating...")
+    # print("Authenticating...")
     service = auth()
-    print("Authentication Successful!")
+    # print("Authentication Successful!")
 
-    print("Retrieving Data File...")
+    # print("Retrieving Data File...")
     data = get_data()
-    print("Data File Retrieved Successfully!")
+    # print("Data File Retrieved Successfully!")
 
-    for d in data:        
-        print("Processing Folder ID:", d["folderId"])
+    for d in data:
+        # print("Processing Folder ID:", d["folderId"])
         items = get_files(service, d)
-        fileGroups, count = parse_files(items, d['formats'])
-        print("Loaded", count, "items!")
+        fileList, count = parse_files(items, d['formats'], d['folderId'])
+        # print("Loaded", count, "items!")
 
-        print("Combining PDFs...")
-        
+        # print("Combining PDFs...")
+    combine_pdfs(service, fileList)
 
 
 def auth():
@@ -63,80 +63,179 @@ def get_data():
 
 
 def get_files(service, data):
-    # Find the given folder
-    folderId = data['folderId']
+    # Initiate items and pageToken vars
+    items = []
+    pageToken = True
 
     # Use API to get the list of files in the folder
-    # pylint: disable=no-member
-    results = service.files().list(
-        fields="nextPageToken, files(id, name, parents, mimeType, modifiedTime)",
-        spaces='drive',
-        q=f"mimeType = 'application/pdf' and '{folderId}' in parents").execute()
-    items = results.get('files', [])
+    while pageToken:
+        # Check for placeholder pageToken and call the API
+        results = call_list_api(service, data['folderId'], pageToken if pageToken != True else None)
 
+        # Add the files to the list of items
+        items += results.get('files', [])
+
+        # Check to see if there are more files
+        if "nextPageToken" in results:
+            pageToken = results.get('nextPageToken')
+        else:
+            pageToken = False
+
+    # Return an empty array if there are no items
     if not items:
+        print("There are no items in this folder!")
         return []
     else:
         return items
 
 
-def parse_files(items, formats):
+def call_list_api(service, folderId, pageToken):
+    # pylint: disable=no-member
+    return service.files().list(
+        fields="nextPageToken, files(id, name, parents, mimeType, modifiedTime)",
+        spaces='drive',
+        pageSize=1000,
+        q=f"mimeType = 'application/pdf' and '{folderId}' in parents",
+        pageToken=pageToken).execute()
+
+
+def parse_files(items, formats, folderId):
+    # Initialize the counter and main files array
     count = 0
-    fileGroups = []
+    fileList = []
+
+    # Iterate through the list of format objects
     for f in formats:
+        # Create the regex objects
         mp = re.compile(f["master"])
         p = re.compile(f["regex"])
+
+        # Check if there are subgroups
         if f["subgroup"] == -1:
+            # If no subgroups
+            # Initiate dictionary and masterfile vars
             d = {}
             masterFile = None
+
+            # Iterate through the list of items found
             for i in items:
+                # Check if the current item matches the regexes
                 m = p.match(i["name"])
                 ma = mp.match(i["name"])
+
+                # Add to the list of files or set it as the masterfile
+                # if name matches the search terms
                 if m:
-                    d[m.groups()[0]] = i["id"]
+                    d[int(m.groups()[0])] = i["id"]
                 if ma:
                     masterFile = i["id"]
+
+            # If there are matches
+            # Put the files into an OrderedDict
             if len(d) != 0:
                 od = OrderedDict(sorted(d.items()))
-                fileGroups.append({
-                    'subgroup': False,
-                    'files': list(od.values()), 
-                    'master': masterFile
+
+                # Get the sorted list of files
+                vals = list(od.values())
+
+                # If the masterfile exists, insert it to the front
+                # Or else insert 'None'
+                if masterFile:
+                    vals.insert(0, masterFile)
+                else:
+                    vals.insert(0, None)
+
+                # Append the current file group to the dictionary
+                fileList.append({
+                    'files': vals,
+                    'parent': folderId,
+                    'master': f["master"]
                 })
-                count += len(od)
+
+                # Increment the count as the number of files
+                count += len(vals)
+
+        # If there are subgroups
         else:
+            # Check which capturing group is the subgroup
+            # and switch the variables if needed
             a, b = 0, 1
             if f["subgroup"] == 1:
                 a, b = b, a
+
+            # Initialize the dictionary and masterfile dictionary
             d = {}
             masterFiles = {}
+
+            # Iterate through all the items in the found
             for i in items:
+                # Check if the current item matches the regexes
                 m = p.match(i["name"])
                 ma = mp.match(i["name"])
+
+                # If the file name matches,
+                # add it to the correct subgroup
                 if m:
+                    # Get the capturing groups
                     g = m.groups()
-                    if g[a] not in d:
-                        d[g[a]] = {}
-                    d[g[a]][g[b]] = i["id"]
-                    count += 1
+
+                    # Check to see if the subgroup exists
+                    # and add initialize that dictionary if not
+                    if int(g[a]) not in d:
+                        d[int(g[a])] = {}
+
+                    # Add the item to the subgroup
+                    d[int(g[a])][int(g[b])] = i["id"]
+
+                # If the file name matches the masterfile pattern
+                # add it to the masterfile dictionary
                 if ma:
-                    masterFiles[ma.groups()[0]] = i["id"]
-            d2 = {}
+                    masterFiles[int(ma.groups()[0])] = i["id"]
+
+            # Iterate through the items in the first dictionary
             for item in d.items():
+
+                # If the item has at least one file,
+                # put the files into an OrderedDict
                 if len(item) != 0:
                     od = OrderedDict(sorted(item[1].items()))
-                    d2[item[0]] = list(od.values())
-            if len(d2) != 0:
-                fileGroups.append({
-                    'subgroup': True,
-                    'files': d2, 
-                    'master': masterFiles
-                })
-    return (fileGroups, count)
+
+                    # Get the sorted list of files
+                    vals = list(od.values())
+
+                    # If the masterfile exists, insert it to the front
+                    # Or else insert 'None'
+                    if item[0] in masterFiles:
+                        vals.insert(0, masterFiles[item[0]])
+                    else:
+                        vals.insert(0, None)
+
+                    # Append the current file group to the dictionary
+                    fileList.append({
+                        'files': vals,
+                        'parent': folderId,
+                        'master': f["master"],
+                        'subname': item[0]
+                    })
+
+                    # Increment the count as the number of files
+                    count += len(vals)
+
+    # Return the final fileList and count of files
+    return (fileList, count)
 
 
-def combine_pdfs(service, fileGroups):
-    print(fileGroups)
+def combine_pdfs(service, fileList):
+    for group in fileList:
+        print(group)
+
+
+def download_pdf(service, fileId):
+    print(fileId)
+
+
+def merge_pdf(service, path):
+    print(path)
 
 
 if __name__ == '__main__':
